d69a25c6b3dcdf7b497d868d1405d713aacd2e78
LeeJinyoung218 <amy021899@gmail.com>
2024-04-17 16:25:55 +0900

    feat: YamlPV, YamlPVC와 필요 컴포넌트 구현

---
+++ src/components/formtoyaml/YamlPVC.jsx
@@ -0,0 +1,120 @@
+import React, {useState, useEffect} from 'react';
+import AccordionSummary from "./AccordionSummary.jsx";
+import AccordionDetails from "./AccordionDetails.jsx";
+import InputBox from "./InputBox.jsx";
+import TextField from "./TextField.jsx";
+import DeleteButton from "./DeleteButton.jsx";
+import AddButton from "./AddButton.jsx";
+import Accordion from "./Accordion.jsx";
+import Radio from "./Radio.jsx";
+import {Typography} from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import '../../styles/FORMTOYAML.css';
+import StorageInputBox from "./StorageInputBox.jsx";
+
+const YamlPvc = () => {
+    const [metadataName, setMetadataName] = useState("");
+    const [labels, setLabels] = useState([]);
+    const [storage, setStorage] = useState("");
+    const [accessModes, setAccessModes] = useState("ReadWriteOnce");
+
+    const yaml = `
+    apiVersion: v1
+    kind: PersistentVolumeClaim 
+    metadata:
+        name: ${metadataName}
+      labels:
+    ${labels.filter(i => i.name !== "" && i.value !== "").map(i => "    " + i.name + ": " + i.value).join("\n")}
+    spec:
+      resources: 
+        requests:
+          storage: ${storage}
+      accessModes:
+        - ${accessModes}
+    `
+
+    useEffect(() => {
+        console.log(yaml);
+        // onDataChange(yaml);
+    }, [yaml])
+
+    const onClickAddLabel = () => {
+        setLabels([...labels, {"name": "", "value":""}]);
+    }
+    const onClickDeleteLabel = (index) => {
+        setLabels(labels.filter((_, idx) => idx !== index));
+    }
+
+    return (
+        <div style={{width: "600px"}}>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>metadata</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className="detail-container">
+                        <InputBox name="name" setter={setMetadataName}/>
+                        <p>labels</p>
+                        <div className="labels">
+                            {labels.map((label, index) => {
+                                return (
+                                    <div key={index} style={{ display: 'flex', justifyContent: "space-between", alignItems: "center"}}>
+                                        <TextField onChange={
+                                            (e) => {
+                                                setLabels(labels.map((item, idx) =>
+                                                    idx === index ? {...item, 'name': e.target.value} : item
+                                                ));
+                                            }
+                                        } id="standard-basic" variant="standard" label="name" placeholder="name"/>
+                                        <TextField onChange={
+                                            (e) => {
+                                                setLabels(labels.map((item, idx) =>
+                                                    idx === index ? {...item, 'value': e.target.value} : item
+                                                ));
+                                            }
+                                        }
+                                                   id="standard-basic" variant="standard" label="value" placeholder="value"/>
+                                        <DeleteButton onClick={() => onClickDeleteLabel(index)}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <AddButton onClick={onClickAddLabel} >Add Labels</AddButton>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary>
+                    <Typography>spec</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <div className="detail-container">
+                        <p>resources</p>
+                        <div className="detail-container">
+                            <p>requests</p>
+                            <div className="detail-container">
+                                <StorageInputBox name="storage" setter={setStorage} max={0.5}/>
+                            </div>
+                        </div>
+                        <p>accessModes</p>
+                        <FormControl onChange={(e) => setAccessModes(e.target.value)}>
+                            <RadioGroup
+                                aria-labelledby="demo-radio-buttons-group-label"
+                                defaultValue="ReadWriteOnce"
+                                name="radio-buttons-group"
+                            >
+                                <FormControlLabel value="ReadWriteOnce" control={<Radio />} label="ReadWriteOnce" />
+                                <FormControlLabel value="ReadOnlyMany" control={<Radio />} label="ReadOnlyMany" />
+                                <FormControlLabel value="ReadWriteMany" control={<Radio />} label="ReadWriteMany" />
+                            </RadioGroup>
+                        </FormControl>
+                    </div>
+                </AccordionDetails>
+            </Accordion>
+        </div>
+    );
+};
+
+export default YamlPvc;
