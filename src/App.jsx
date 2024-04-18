8673d52a895b1ecee8ea07b8bb68850bbab6c029
LeeJinyoung218 <amy021899@gmail.com>
2024-04-18 14:16:50 +0900

    feat: App.jsx에 Router 설정, 페이지 연결 BBB-29

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Main from "./pages/Main.jsx";
+import FormToYaml from "./pages/FormToYaml.jsx";
+import Configure from "./pages/cd/Configure.jsx";
+import CDDashboard from "./pages/cd/Dashboard.jsx";
+import RepoList from "./pages/cd/RepoList.jsx";
+import ClusterList from "./pages/monitoring/ClusterList.jsx";
+import MonitoringDashboard from "./pages/monitoring/Dashboard.jsx";
+import Token from "./pages/monitoring/Token.jsx";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/formToYaml" element={<FormToYaml />} />
+          <Route path="/cd/configure" element={<Configure />} />
+          <Route path="/cd/dashboard" element={<CDDashboard />} />
+          <Route path="/cd/repoList" element={<RepoList />} />
+          <Route path="/monitoring/clusterList" element={<ClusterList />} />
+          <Route path="/monitoring/dashboard" element={<MonitoringDashboard />} />
+          <Route path="/monitoring/token" element={<Token />} />
+        </Routes>
+      </Router>
     </>
   )
 }
