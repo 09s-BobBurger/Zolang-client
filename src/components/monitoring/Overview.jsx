006cb0f6c49d02ba0bc68a560ba6613e079b6cd8
minsun <elinnane0@gmail.com>
2024-05-03 14:23:45 +0900

    feat: Overview Pods list 구현 BBB-59

--- src/components/monitoring/Overview.jsx
+++ src/components/monitoring/Overview.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Chart from "./Chart.jsx";
 import Nodes from "./Nodes.jsx";
+import Pods from "./Pods.jsx";
 
 const Overview = () => {
     return (
         <div className="overview-content">
-            <div style={{flex: "1"}}>
+            <div style={{flex: "1", display: "flex", gap: "33px"}}>
                 <Chart
                     title="CPU allocation"
                     values={[
@@ -14,6 +15,25 @@ const Overview = () => {
                     ]}
                     fullValue={2.0}
                 />
+                <Chart
+                    title="Memory allocation"
+                    values={[
+                        { name: "Requests", value: 526.00 },
+                        { name: "Limits", value: 404.00 },
+                    ]}
+                    fullValue={2.0}
+                />
+                <Chart
+                    title="Pods allocation"
+                    values={[
+                        { name: "Requests", value: 24 },
+                        { name: "Limits", value: 100 },
+                    ]}
+                    fullValue={2.0}
+                />
+            </div>
+            <div style={{flex: "1"}}> 
+            <Pods />
             </div>
             <div style={{flex: "1"}}> 
             <Nodes />
