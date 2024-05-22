ad55bae541aa543ffb44661e6f11d14d3ade34ce
LeeJinyoung218 <amy021899@gmail.com>
2024-05-22 15:16:17 +0900

    feat: Monitoring Dashboard - Pod List Mocking Api 연결, namespace에 따른 구별 완료

--- src/components/monitoring/workloads/Pods/Pods.jsx
+++ src/components/monitoring/workloads/Pods/Pods.jsx
@@ -1,71 +1,55 @@
 import React, { useState, useEffect } from 'react';
 import PodDetail from "./PodDetail.jsx";
 import PodsList from "./PodsList.jsx";
+import {useSelector} from "react-redux";
+import {customizedAxios as axios} from "../../../../util/customizedAxios.js";
+import loginUtil from "../../../../util/login.js";
 
 const Pods = () => {
     const [podsData, setPodsData] = useState({totalUsage: [], pods: []});
     const [selectedPod, setSelectedPod] = useState(null);
+    const clusterId = useSelector((state) => state.cluster.clusterId);
+    const namespace = useSelector((state) => state.namespace.namespace);
+
+    const loadData = () => {
+        if (namespace === "All") {
+            axios
+                .get(`/api/v1/cluster/${clusterId}/workload/pods`,
+                    {
+                        headers: {
+                            "Authorization": "Bearer " + loginUtil.getAccessToken(),
+                        }
+                    }
+                )
+                .then((res) => {
+                    setPodsData(res.data.data);
+                }).catch((err) => {
+                console.log(err)
+            })
+        } else {
+            axios
+                .get(`/api/v1/cluster/${clusterId}/workload/pods/namespace?namespace=${namespace}`,
+                    {
+                        headers: {
+                            "Authorization": "Bearer " + loginUtil.getAccessToken(),
+                        }
+                    }
+                )
+                .then((res) => {
+                    setPodsData(res.data.data);
+                }).catch((err) => {
+                console.log(err)
+            })
+        }
+    }
 
     useEffect(() => {
-        setPodsData({
-            success: true,
-            data: {
-                totalUsage: [
-                    { time: "3:58", cpuUsage: 0.592, memoryUsage: 2044420096 },
-                    { time: "3:59", cpuUsage: 0.586, memoryUsage: 1844912128 },
-                    { time: "4:00", cpuUsage: 0.726, memoryUsage: 2045956096 },
-                    { time: "4:01", cpuUsage: 0.558, memoryUsage: 2053005312 },
-                    { time: "4:02", cpuUsage: 0.523, memoryUsage: 1850400768 },
-                    { time: "4:03", cpuUsage: 0.532, memoryUsage: 2035978240 },
-                    { time: "4:04", cpuUsage: 0.687, memoryUsage: 2144362496 },
-                    { time: "4:05", cpuUsage: 0.595, memoryUsage: 2039218176 },
-                    { time: "4:06", cpuUsage: 0.717, memoryUsage: 2032902144 },
-                    { time: "4:07", cpuUsage: 0.62, memoryUsage: 2042241024 },
-                    { time: "4:08", cpuUsage: 0.618, memoryUsage: 2033393664 },
-                    { time: "4:09", cpuUsage: 0.532, memoryUsage: 2038845440 },
-                    { time: "4:10", cpuUsage: 0.665, memoryUsage: 2063880192 }
-                ],
-                pods: [
-                    {
-                        name: "nginx-ingress-microk8s-controller-4n4rq",
-                        namespace: "ingress",
-                        images: ["registry.k8s.io/ingress-nginx/controller:v1.5.1"],
-                        labels: {
-                            "controller-revision-hash": "7466d5f4cb",
-                            name: "nginx-ingress-microk8s",
-                            "pod-template-generation": "10"
-                        },
-                        node: "instance-20230502-0040",
-                        status: "Running",
-                        restartCount: [21],
-                        usage: {
-                            time: "4:11",
-                            cpuUsage: 0.002,
-                            memoryUsage: 86224896
-                        },
-                        metrics: [
-                            { time: "3:58", cpuUsage: 0.002, memoryUsage: 82481152 },
-                            { time: "3:59", cpuUsage: 0.002, memoryUsage: 82137088 },
-                            { time: "4:00", cpuUsage: 0.001, memoryUsage: 82149376 },
-                            { time: "4:01", cpuUsage: 0.002, memoryUsage: 86355968 },
-                            { time: "4:02", cpuUsage: 0.003, memoryUsage: 86466560 },
-                            { time: "4:03", cpuUsage: 0.003, memoryUsage: 86458368 },
-                            { time: "4:04", cpuUsage: 0.003, memoryUsage: 86507520 },
-                            { time: "4:05", cpuUsage: 0.002, memoryUsage: 86614016 },
-                            { time: "4:06", cpuUsage: 0.001, memoryUsage: 84226048 },
-                            { time: "4:07", cpuUsage: 0.002, memoryUsage: 86355968 },
-                            { time: "4:08", cpuUsage: 0.001, memoryUsage: 84217856 },
-                            { time: "4:09", cpuUsage: 0.002, memoryUsage: 86224896 },
-                            { time: "4:10", cpuUsage: 0.002, memoryUsage: 84217856 }
-                        ],
-                        age: "22 day",
-                        creationDateTime: "2024. 04. 26. 오후 15:53:49"
-                    }
-                ]
-            },
-            error: null
-        }.data)
+        loadData();
     }, []);
+
+    useEffect(() => {
+        loadData();
+    }, [namespace]);
 
     return (
         <div className="overview-content">
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -213,4 +213,438 @@ export const handlers = [
             "error": null
         })
     }),
+
+    // get pods of cluster
+    http.get(`${baseURL}/api/v1/cluster/:clusterId/workload/pods`, async ({params}) => {
+        const { clusterId } = params;
+        return HttpResponse.json({
+            "success": true,
+            "data": {
+                "totalUsage": [
+                    {
+                        "time": "3:58",
+                        "cpuUsage": 0.592,
+                        "memoryUsage": 2044420096
+                    },
+                    {
+                        "time": "3:59",
+                        "cpuUsage": 0.586,
+                        "memoryUsage": 1844912128
+                    },
+                    {
+                        "time": "4:00",
+                        "cpuUsage": 0.726,
+                        "memoryUsage": 2045956096
+                    },
+                    {
+                        "time": "4:01",
+                        "cpuUsage": 0.558,
+                        "memoryUsage": 2053005312
+                    },
+                    {
+                        "time": "4:02",
+                        "cpuUsage": 0.523,
+                        "memoryUsage": 1850400768
+                    },
+                    {
+                        "time": "4:03",
+                        "cpuUsage": 0.532,
+                        "memoryUsage": 2035978240
+                    },
+                    {
+                        "time": "4:04",
+                        "cpuUsage": 0.687,
+                        "memoryUsage": 2144362496
+                    },
+                    {
+                        "time": "4:05",
+                        "cpuUsage": 0.595,
+                        "memoryUsage": 2039218176
+                    },
+                    {
+                        "time": "4:06",
+                        "cpuUsage": 0.717,
+                        "memoryUsage": 2032902144
+                    },
+                    {
+                        "time": "4:07",
+                        "cpuUsage": 0.62,
+                        "memoryUsage": 2042241024
+                    },
+                    {
+                        "time": "4:08",
+                        "cpuUsage": 0.618,
+                        "memoryUsage": 2033393664
+                    },
+                    {
+                        "time": "4:09",
+                        "cpuUsage": 0.532,
+                        "memoryUsage": 2038845440
+                    },
+                    {
+                        "time": "4:10",
+                        "cpuUsage": 0.665,
+                        "memoryUsage": 2063880192
+                    }
+                ],
+                "pods": [
+                    {
+                        "name": "nginx-ingress-microk8s-controller-4n4rq",
+                        "namespace": "ingress",
+                        "images": [
+                            "registry.k8s.io/ingress-nginx/controller:v1.5.1"
+                        ],
+                        "labels": {
+                            "controller-revision-hash": "7466d5f4cb",
+                            "name": "nginx-ingress-microk8s",
+                            "pod-template-generation": "10"
+                        },
+                        "node": "instance-20230502-0040",
+                        "status": "Running",
+                        "restartCount": [
+                            21
+                        ],
+                        "usage": {
+                            "time": "4:11",
+                            "cpuUsage": 0.002,
+                            "memoryUsage": 86224896
+                        },
+                        "metrics": [
+                            {
+                                "time": "3:58",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 82481152
+                            },
+                            {
+                                "time": "3:59",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 82137088
+                            },
+                            {
+                                "time": "4:00",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 82149376
+                            },
+                            {
+                                "time": "4:01",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 86355968
+                            },
+                            {
+                                "time": "4:02",
+                                "cpuUsage": 0.003,
+                                "memoryUsage": 86466560
+                            },
+                            {
+                                "time": "4:03",
+                                "cpuUsage": 0.003,
+                                "memoryUsage": 86458368
+                            },
+                            {
+                                "time": "4:04",
+                                "cpuUsage": 0.003,
+                                "memoryUsage": 86507520
+                            },
+                            {
+                                "time": "4:05",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 86614016
+                            },
+                            {
+                                "time": "4:06",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 84226048
+                            },
+                            {
+                                "time": "4:07",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 86355968
+                            },
+                            {
+                                "time": "4:08",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 84217856
+                            },
+                            {
+                                "time": "4:09",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 86224896
+                            },
+                            {
+                                "time": "4:10",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 84217856
+                            }
+                        ],
+                        "age": "22 day",
+                        "creationDateTime": "2024. 04. 26. 오후 15:53:49"
+                    }
+                ],
+                "error": null
+            }
+        })
+    }),
+
+    // get pods of cluster with namespace
+    http.get(`${baseURL}/api/v1/cluster/:clusterId/workload/pods/namespace`, async ({params, request}) => {
+        const { clusterId } = params;
+        const namespace = new URL(request.url).searchParams.get('namespace');
+        return HttpResponse.json({
+            "success": true,
+            "data": {
+                "totalUsage": [
+                    {
+                        "time": "3:59",
+                        "cpuUsage": 0.004,
+                        "memoryUsage": 141967360
+                    },
+                    {
+                        "time": "4:00",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 141967360
+                    },
+                    {
+                        "time": "4:01",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 141967360
+                    },
+                    {
+                        "time": "4:02",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 141967360
+                    },
+                    {
+                        "time": "4:03",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 124542976
+                    },
+                    {
+                        "time": "4:04",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:05",
+                        "cpuUsage": 0.004,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:06",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127156224
+                    },
+                    {
+                        "time": "4:07",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:08",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:09",
+                        "cpuUsage": 0.004,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:10",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127148032
+                    },
+                    {
+                        "time": "4:11",
+                        "cpuUsage": 0.002,
+                        "memoryUsage": 127148032
+                    }
+                ],
+                "pods": [
+                    {
+                        "name": "grafana-657c7689bd-hxq6d",
+                        "namespace": "monitoring",
+                        "images": [
+                            "grafana/grafana:latest"
+                        ],
+                        "labels": {
+                            "app": "grafana",
+                            "pod-template-hash": "657c7689bd"
+                        },
+                        "node": "instance-20230123-2111",
+                        "status": "Running",
+                        "restartCount": [
+                            3
+                        ],
+                        "usage": {
+                            "time": "4:12",
+                            "cpuUsage": 0.002,
+                            "memoryUsage": 63627264
+                        },
+                        "metrics": [
+                            {
+                                "time": "3:59",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:00",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:01",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:02",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:03",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 62271488
+                            },
+                            {
+                                "time": "4:04",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:05",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:06",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63578112
+                            },
+                            {
+                                "time": "4:07",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:08",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:09",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:10",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:11",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            }
+                        ],
+                        "age": "2 day",
+                        "creationDateTime": "2024. 05. 16. 오전 06:51:25"
+                    },
+                    {
+                        "name": "prometheus-server-0",
+                        "namespace": "monitoring",
+                        "images": [
+                            "prom/prometheus:latest"
+                        ],
+                        "labels": {
+                            "app": "prometheus",
+                            "apps.kubernetes.io/pod-index": "0",
+                            "controller-revision-hash": "prometheus-server-566fc6784b",
+                            "statefulset.kubernetes.io/pod-name": "prometheus-server-0"
+                        },
+                        "node": "instance-20230502-0040",
+                        "status": "Running",
+                        "restartCount": [
+                            0
+                        ],
+                        "usage": {
+                            "time": "4:12",
+                            "cpuUsage": 0.001,
+                            "memoryUsage": 36753408
+                        },
+                        "metrics": [
+                            {
+                                "time": "3:59",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:00",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:01",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:02",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 70983680
+                            },
+                            {
+                                "time": "4:03",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 62271488
+                            },
+                            {
+                                "time": "4:04",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:05",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:06",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63578112
+                            },
+                            {
+                                "time": "4:07",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:08",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:09",
+                                "cpuUsage": 0.002,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:10",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            },
+                            {
+                                "time": "4:11",
+                                "cpuUsage": 0.001,
+                                "memoryUsage": 63574016
+                            }
+                        ],
+                        "age": "2 day",
+                        "creationDateTime": "2024. 05. 16. 오후 13:40:06"
+                    }
+                ]
+            },
+            "error": null
+        })
+    }),
 ]
