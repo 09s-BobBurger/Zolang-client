c5332f2af85857e851c2736d2fc85ef28f777ef9
LeeJinyoung218 <amy021899@gmail.com>
2024-05-22 16:28:28 +0900

    feat: Monitoring Dashboard Workloads 일부에서 공통적으로 사용할 테이블 컴포넌트로 구현

---
+++ src/components/monitoring/workloads/ControllerTable.jsx
@@ -0,0 +1,138 @@
+import React from 'react';
+import Label from "../nodes/Label.jsx";
+import Status from "../../icon/Status.jsx";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+
+// demonSet, deployment, statefulSet, ReplicasSet, Job 모두 list table 형식이 같음(cronjob만 형식 다름)
+const ControllerTable = ({data, onClickRow}) => {
+    return (
+        <div
+            style={{
+                padding: "15px",
+                outline: "1px solid #ABAFBD",
+                borderRadius: "10px",
+                background: "#2E3240",
+                justifyContent: "center",
+                overflow: "auto",
+            }}
+        >
+            <div
+                style={{
+                    color: "#ffffff",
+                    padding: "10px",
+                    paddingTop: "0px",
+                    paddingBottom: "0px",
+                    paddingLeft: "15px",
+                    justifyContent: "space-between",
+                    display: "flex",
+                }}
+            >
+                        <span
+                            style={{
+                                paddingTop: "10px",
+                                fontSize: "24px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            List
+                        </span>
+            </div>
+            <hr
+                style={{
+                    width: "98%",
+                    border: 0,
+                    height: "1px",
+                    backgroundColor: "#474B59",
+                    marginBottom: "15px",
+                }}
+            />
+
+            <div className="moni-dashboard-nodes" style={{width: '100%'}}>
+                <TableContainer>
+                    <Table
+                        sx={{minWidth: 650, color: "#ffffff"}}
+                        aria-label="simple table"
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>
+                                    Name
+                                </TableCell>
+                                <TableCell align="center">
+                                    Namespace
+                                </TableCell>
+                                <TableCell align="left">
+                                    Images
+                                </TableCell>
+                                <TableCell align="left">
+                                    Labels
+                                </TableCell>
+                                <TableCell align="center">
+                                    Pods
+                                </TableCell>
+                                <TableCell align="center">
+                                    Age
+                                </TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {data.map((item) => (
+                                <TableRow
+                                    key={item.name}
+                                    onClick={() =>
+                                        onClickRow(item.name)
+                                    }
+                                    sx={{
+                                        "&:last-child td, &:last-child th":
+                                            {
+                                                border: 0,
+                                            },
+                                    }}
+                                >
+                                    <TableCell
+                                        component="th"
+                                        scope="row"
+                                    >
+                                        {item.name}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {item.namespace}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px'}}>
+                                            {item.images.slice(0, 3).map(image => {
+                                                return <Label name={image}/>
+                                            })}
+                                            {item.images.length > 3 && <Label name="..." />}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        <div style={{ display: 'flex', flexWrap: 'wrap', gap: '5px'}}>
+                                            {Object.keys(item.labels).slice(0, 3).map((key) => {
+                                                return <Label name={key + ":" + item.labels[key]}/>
+                                            })}
+                                            {Object.keys(item.labels).length > 3 && <Label name="..." />}
+                                        </div>
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {item.replicas} / {item.readyReplicas}
+                                    </TableCell>
+                                    <TableCell align="center">
+                                        {item.age}
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        </div>
+    );
+};
+
+export default ControllerTable;
