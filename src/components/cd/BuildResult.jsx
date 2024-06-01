8715a7d06fb4e9bb992064b960f605e189fb4cf1
LeeJinyoung218 <amy021899@gmail.com>
2024-06-01 16:41:43 +0900

    feat: cd bulid result 페이지 뷰 구현

---
+++ src/components/cd/BuildResult.jsx
@@ -0,0 +1,131 @@
+import React, {useEffect, useState} from 'react';
+import Button from "@mui/material/Button";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import '../../styles/MONITORING.css';
+import {customizedAxios as axios} from "../../util/customizedAxios.js";
+import BuildingIcon from "../icon/BuildingIcon.jsx";
+
+const BuildResult = () => {
+    const repositoryId = 1;
+    const [results, setResults] = useState([]);
+
+    const loadData = () => {
+        axios
+            .get(`/api/v1/cicd/${repositoryId}`)
+            .then(res => {
+                setResults(res.data.data);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    useEffect(() => {
+        loadData();
+    }, [])
+
+    return (
+        <div
+            style={{
+                background: "#2E3240",
+                top: "68px",
+                position: "fixed",
+                boxSizing: "border-box",
+                width: "100vw",
+                height: "calc(100% - 68px)",
+                overflow: "auto",
+                padding: "30px 40px"
+            }}
+        >
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginBottom: "20px"
+                }}
+            >
+                <span
+                    style={{
+                        fontSize: "2.5rem",
+                        color: "white",
+                    }}
+                >
+                    Repository Name
+                </span>
+                <Button
+                    style={{
+                        width: "fit-content",
+                        height: "fit-content",
+                        margin: 0,
+                        background: "transparent",
+                        outline: "none",
+                    }}
+                >
+                    <img width="40px" src="../../../setting.svg" alt="edit build setting"/>
+                </Button>
+            </div>
+            <div className="moni-dashboard-nodes"
+                style={{
+                    width: "100%"
+                }}
+            >
+                <TableContainer>
+                    <Table
+                        sx={{ color: "#ffffff" }}
+                        aria-label="simple table"
+                    >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center" width="10%">Build Result</TableCell>
+                                <TableCell align="center" width="10%">No.</TableCell>
+                                <TableCell align="center">Last Commit Message</TableCell>
+                                <TableCell align="center" width="20%">Build Time</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {
+                                results.length > 0 &&
+                                    results.map((item, index) => {
+                                        const createdAt = item.createdAt;
+
+                                        return (
+                                            <TableRow key={index} sx={{ height: "60px"}}>
+                                                <TableCell>
+                                                    {item.buildStatus === "success" ?
+                                                        <img width="30px" src="../../../success-svgrepo-com.svg"  alt="build succeed"/>
+                                                        : item.buildStatus === "building" ?
+                                                            <BuildingIcon />
+                                                            : <img  width="30px" src="../../../failed.svg" alt="build failed"/>
+                                                    }
+                                                </TableCell>
+                                                <TableCell>{item.buildNumber}</TableCell>
+                                                <TableCell>{item.lastCommitMessage}</TableCell>
+                                                <TableCell>
+                                                    {createdAt.slice(0, 3).join(".") + " "
+                                                        + createdAt.slice(3, createdAt.length - 1).join(":")}
+                                                </TableCell>
+                                            </TableRow>
+                                        )
+                                    })
+                            }
+                        </TableBody>
+                    </Table>
+                    {
+                        results.length === 0 &&
+                        <div style={{textAlign: "-webkit-center", paddingTop: "50px"}}>
+                            <img src=".././텅.svg" width="150px" alt="이미지"/>
+                        </div>
+                    }
+                </TableContainer>
+            </div>
+        </div>
+    );
+};
+
+export default BuildResult;
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -3204,4 +3204,58 @@ export const handlers = [
             });
         }
     ),
+
+    http.get(`${baseURL}/api/v1/cicd/:repository_id`, ({params}) => {
+        const {repository_id} = params;
+        return HttpResponse.json(
+            {
+                "success": true,
+                "data": [
+                    {
+                        "buildNumber": 1,
+                        "lastCommitMessage": "init",
+                        "buildStatus": "success", // "building", "failed"
+                        "createdAt": [
+                            2024,
+                            5,
+                            28,
+                            14,
+                            37,
+                            37,
+                            871071000
+                        ]
+                    },
+                    {
+                        "buildNumber": 2,
+                        "lastCommitMessage": "test2",
+                        "buildStatus": "building", // "building", "failed"
+                        "createdAt": [
+                            2024,
+                            5,
+                            28,
+                            14,
+                            37,
+                            37,
+                            871071000
+                        ]
+                    },
+                    {
+                        "buildNumber": 3,
+                        "lastCommitMessage": "test3",
+                        "buildStatus": "failed", // "building", "failed"
+                        "createdAt": [
+                            2024,
+                            5,
+                            28,
+                            14,
+                            37,
+                            37,
+                            871071000
+                        ]
+                    }
+                ],
+                "error": null
+            }
+        )
+    })
 ];
